d2069d7d05397c8c907de12603e4d7aaa4564aab
PaulJulio <paulhulett@gmail.com>
2015-12-31 18:57:31 -0800

    Reorder app middleware invocation
    Getting a Slim Body instead of a JSON Stream. Trying to reorder for
    correct behavior.

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $app = new \Slim\App();
 // Reminder: the request is processed from the bottom up,
 // the response is processed from the top down.
 
-$app->add(\PaulJulio\AmazonEchoGrover\Grover::class);
 $app->add(\PaulJulio\StreamJSON\SlimMiddleware::class);
+$app->add(\PaulJulio\AmazonEchoGrover\Grover::class);
 
 $app->run();
--- src/Grover.php
+++ src/Grover.php
@@ -2,7 +2,13 @@
 namespace PaulJulio\AmazonEchoGrover;
 
 class Grover {
-
+    /**
+     * @param  \Psr\Http\Message\ServerRequestInterface $request  PSR7 request
+     * @param  \Psr\Http\Message\ResponseInterface      $response PSR7 response
+     * @param  callable                                 $next     Next middleware
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
     public function __invoke($request, $response, $next) {
         $response = $next($request, $response);
         $body = $response->getBody();
